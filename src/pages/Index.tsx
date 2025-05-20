81f310972b3f76e167e45b227c1c1173a8e95db7
charly7978 <Carlos_ameghino@hotmail.com>
2025-05-20 14:31:12 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -605,7 +605,7 @@ const Index = () => {
 
           <div className="flex-1">
             <PPGSignalMeter 
-              value={heartbeatSignal * 10}
+              value={heartbeatSignal * 20}
               quality={lastSignal?.quality || 0}
               isFingerDetected={lastSignal?.fingerDetected || false}
               onStartMeasurement={startMonitoring}
